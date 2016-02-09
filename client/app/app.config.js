cdbb8cd2cd019b5489819859ba6be03c22c96403
tnr2394 <tirthrajbarot2394@gmail.com>
2016-02-09 13:31:56 +0530

    Conflicts Resolved

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -11,31 +11,15 @@ angular.module("rotaract")
 					}
 				}
 			})
-<<<<<<< HEAD
 			.state('guest',{
 				url : '/guest',
-=======
-			.state('treasurer',{
-				url : '/treasurer',
 				views : {
-					'top' : {	templateUrl : 'treasurer/view/menu.html'},
+					'top' : {	templateUrl : 'guest/view/menu.html'},
 					'main' : {	template : '<ui-view></ui-view>', 
 								controller : function () {
 								}
 							}
 					
-				}
-			})
-			.state('postHolders',{
-				url : '/postHolders',
-				
->>>>>>> eea28a977589b6cf36890bc3f47aa1db5b3ea801
-				views : {
-					top : {templateUrl : 'guest/view/menu.html'},
-					main : {
-						template : "<ui-view></ui-view>",
-						controller : function(){}
-					}
 				}
 			})
 
@@ -78,16 +62,7 @@ angular.module("rotaract")
 			});
 
 
-<<<<<<< HEAD
 		$urlRouterProvider.otherwise('/guest/');
 	}]).run(function ($state,$rootScope) {
     $rootScope.$state = $state;
 });
-=======
-		// $urlRouterProvider.otherwise('/');
-	}]).run(['$rootScope','$state',function($rootScope, $state){
-		$rootScope.$state = $state;
-		console.log($rootScope);
-		console.log($rootScope.$state);
-	}]);
->>>>>>> eea28a977589b6cf36890bc3f47aa1db5b3ea801
