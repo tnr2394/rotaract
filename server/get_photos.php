8a6c9e6f64d63197ada97393f9dc14663339324b
tnr2394 <tirthrajbarot2394@gmail.com>
2016-02-09 16:01:54 +0530

    Guest - Events Done

---
+++ server/get_photos.php
@@ -0,0 +1,20 @@
+<?php
+	include("config.php");
+	header("Access-Control-Allow-Origin: *");
+	header("Cache-Control: *"); //HTTP 1.1
+
+	getImages($_GET['id'],$con);
+
+
+
+	function getImages($id,$con){
+		$result = mysqli_query($con,"select * from eventphotos where eventid = '$id' order by id desc") or die(mysqli_error($con));
+		$nara = array();
+		while($y = mysqli_fetch_array($result)){
+			$y['path'] = "http://localhost/Angular/rotaract/server/".$y['path'];
+			array_push($nara,$y);
+		}
+		echo json_encode($nara);
+	}
+
+?>
