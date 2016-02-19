93f94087eb75c4978b7be3bdd917bb22a9183a15
aniketpandya <aniketpandya26@hotmail.com>
2016-02-19 21:36:59 +0530

    add bill

--- client/treasurer/controller/treasurer.addExpense.controller.js
+++ client/treasurer/controller/treasurer.addExpense.controller.js
@@ -1,13 +1,22 @@
 angular.module('rotaract')
 	.controller('addExpense', ['$scope','$state','treasurerFactory', function($scope,$state,treasurerFactory){
-		$scope.addExpense = function(x){
+	$scope.addExpense = function(x){
+		$scope.action = 'http://localhost/Angular/rotaract/server/treasurer/accounts/addBill.php?+'scope.x['name'];
 			treasurerFactory.addExpense(x)
 				.then(function (response) {
 					alert("Expense Added.");
+					scope.dropvisible=true;
 					//$state.go("treasurer.welcome"); 
 				},function (err) {
 					 console.log(err);
 				});
 
 	}
+
+	$scope.navigate=function(){
+		$state.go("treasurer.welcome"); 
+	}
+
+
+
 }]);
--- server/treasurer/accounts/addBill.php
+++ server/treasurer/accounts/addBill.php
@@ -3,10 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Cache-Control: *"); //HTTP 1.1
   include("config.php");    
 
-  if(!file_exists('../../events/'.$_GET['id'])){
-    mkdir('../../events/'.$_GET['id'],0777);
+  if(!file_exists('../../assets/'.$_GET['id'])){
+    mkdir('../../assets/'.$_GET['id'],0777);
   }
-  $upath="../../events/".$_GET['id']."/";
+  $upath="../../assets/".$_GET['id']."/";
   // print_r($_FILES['file']['name']);
   $target = $upath.$_FILES['file']['name'];
   $path=substr($target,6);
