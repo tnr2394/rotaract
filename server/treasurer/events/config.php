95e816126e1c15be782088094fa3a7c9c672a6b3
aniketpandya <aniketpandya26@hotmail.com>
2016-02-20 11:27:53 +0530

    test

--- server/treasurer/events/config.php
+++ server/treasurer/events/config.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('localhost','root','password') or die(mysql_error());
+	$con = mysqli_connect('localhost','root','') or die(mysql_error());
 	$con->set_charset('utf8');
 	mysqli_select_db($con,'rotaract') or die(mysql_error());
 	$GLOBALS['con'] = $con;
